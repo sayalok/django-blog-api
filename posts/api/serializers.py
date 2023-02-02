e5341de6d302c67660eff0e1e66cd8516561c655
sayalok <sayalok@yahoo.com>
2023-02-03 01:00:30 +0600

    auth and create view added

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 from ..models import Post
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+    author_id = serializers.ReadOnlyField(source='author.id')
     class Meta:
         model = Post
-        fields = ['id', 'title', 'url', 'author', 'created']
+        fields = ['id', 'title', 'url', 'author', 'author_id', 'created']
--- posts/api/views.py
+++ posts/api/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from ..models import Post
 from .serializers import PostSerializer
 # Create your views here.
-class PostList(generics.ListAPIView):
+class PostList(generics.ListCreateAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostSerializer
-	
+	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+ 
+	def perform_create(self, serializer):
+		serializer.save(author=self.request.user)
+ 	
