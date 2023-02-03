1a7a67eac46a63c2f639f51937a1110787f4a9f8
sayalok <sayalok@yahoo.com>
2023-02-03 15:14:11 +0600

    vote count add on post list api

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
 
 from ..models import Post
+from votes.models import Vote
 
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
     author_id = serializers.ReadOnlyField(source='author.id')
+    votes = serializers.SerializerMethodField()
     class Meta:
         model = Post
-        fields = ['id', 'title', 'url', 'author', 'author_id', 'created']
+        fields = ['id', 'title', 'url', 'author', 'author_id', 'created', 'votes']
+        
+    def get_votes(self, post):
+        return Vote.objects.filter(post=post).count()
