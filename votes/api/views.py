f6ecc56fa7bcffb8bd882740799fef1e2738f14d
sayalok <sayalok@yahoo.com>
2023-02-03 15:06:37 +0600

    vote delete functionality created

--- votes/api/views.py
+++ votes/api/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, mixins, status
 from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 
 from ..models import Vote
 from posts.models import Post
 from .serializers import VoteSerializer
 
-class VoteCreate(generics.CreateAPIView):
+class VoteCreate(generics.CreateAPIView, mixins.DestroyModelMixin):
 	serializer_class = VoteSerializer
 	permission_classes = [permissions.IsAuthenticated]
  
@@ -21,4 +22,9 @@ class VoteCreate(generics.CreateAPIView):
 			raise ValidationError('You have already voted for this post')
 		serializer.save(voter=self.request.user,post = Post.objects.get(pk=self.kwargs['pk']))
 
- 	
+	def delete(self, request, *args, **kwargs):
+		if self.get_queryset().exists():
+			self.get_queryset().delete()
+			return Response(status=status.HTTP_204_NO_CONTENT)
+
+		raise ValidationError('You have not voted for this post')
